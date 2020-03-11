5a1217e122219bdb69ef0fe4a661ad0071b277df
sabitalizade <54733296+sabitalizade@users.noreply.github.com>
2020-03-12 02:37:36 +0400

    Update baglan.php

--- sadmin/samin/baglan.php
+++ sadmin/samin/baglan.php
@@ -1,5 +1,5 @@
 <?php
-$baglan = new mysqli('eu-cdbr-west-02.cleardb.nett', 'b7e14db0301a4e', '3e585f29', 'sabitalizade');
+$baglan = new mysqli('heroku_e7fe0d236ec8f26', 'b7e14db0301a4e', '3e585f29', 'sabitalizade');
 $baglan->set_charset("utf8");
 if ($baglan->connect_errno) {
  die("mySQL bağlantısı başarısız oldu: " . $baglan->connect_error);
