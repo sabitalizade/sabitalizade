03148cc60238da7d1da1cb665b32ec4125149b80
sabitalizade <54733296+sabitalizade@users.noreply.github.com>
2020-03-12 02:36:12 +0400

    Update baglan.php

--- sadmin/samin/baglan.php
+++ sadmin/samin/baglan.php
@@ -1,20 +1,7 @@
 <?php
-$baglan = new mysqli('localhost', 'root', '', 'sabitalizade');
+$baglan = new mysqli('eu-cdbr-west-02.cleardb.nett', 'b7e14db0301a4e', '3e585f29', 'sabitalizade');
 $baglan->set_charset("utf8");
 if ($baglan->connect_errno) {
  die("mySQL bağlantısı başarısız oldu: " . $baglan->connect_error);
 }
 
-
-$dsn = "mysql:host=localhost;dbname=sabitalizade";
-$user = "root";
-$passwd = "";
-
-$pdo = new PDO($dsn, $user, $passwd);
-
-$stm = $pdo->query("SELECT VERSION()");
-
-$version = $stm->fetch();
-
-//echo $version[0] . PHP_EOL;
-?>
